d26b4e2c025e7a6c1067b291fad49895577fb097
Chris Sanchez <chrisvsanchez@gmail.com>
2020-05-03 00:46:01 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 var name = "Susan"
-var height = "74"
+var height = "70"
 if (name === "Susan"){
   console.log("The name is correct");
 }else{
   console.log("Expected the name to be Susan");
 }
-if (height === "74"){
+if (height === "70"){
   console.log("The height is correct");
 }else{
-  console.log("Expected the height to be 74");
+  console.log("Expected the height to be 70");
 }
 
 // Don't worry about this
